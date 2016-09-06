0977b37b8ac4622e5da29839ae897f1299c147ac
Marek Tokarski <mautokar@gmail.com>
2016-09-06 21:36:04 +0200

    Added WebService

---
+++ WebService/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServicesModels.dto;
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReserveSpot(ReservationDto reservationDto)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
